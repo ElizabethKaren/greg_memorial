85dfc832ace5b32d5f93cdeb516139a436e7157b
Liz00000000000 <elizabeth.michael.karen@gmail.com>
2021-02-11 11:11:33 -0500

    workonLater

--- src/App.js
+++ src/App.js
@@ -2,9 +2,42 @@ import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [pic, setPic] = useState([
+    {
+      image: './images/IMG_5159.PNG'
+    },
+    {
+      image: './images/IMG_5160.PNG'
+    },
+    {
+      image: './images/IMG_5161.PNG'
+    },
+    {
+      image: './images/IMG_5162.PNG'
+    },
+    {
+      image: './images/IMG_5163.PNG'
+    },
+    {
+      image: './images/IMG_5164.PNG'
+    },
+    {
+      image: './images/IMG_5181.PNG'
+    },
+    {
+      image: './images/IMG_5182.PNG'
+    },
+    {
+      image: './images/IMG_5183.PNG'
+    }
+  ])
+
+  console.log(pic)
   return (
     <div className="App">
       <h1>We Love Greg</h1>
+      <img src='./images/IMG_5321.PNG' alt='greg' />
+      { pic.map((greg, index) => <img src={greg.image} alt={index} /> )}
     </div>
   );
 }
